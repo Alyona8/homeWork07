bd48949257042b7392d2a610308a73ee6f84d640
Alyona <alyonushka489@gmail.com>
2022-08-19 20:12:08 +0300

    Выполненное домашнее задание 7

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,17 @@
 public class Main {
     public static void main(String[] args) {
+        String firstName = "Семён";
+        String middleName = "Семёнович";
+        String lastName = "Иванов";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника — " + fullName);
+
+        //Задание 2
+        String upperCase = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + upperCase);
+
+        //Задание 3
+        fullName = fullName.replace("ё", "е");
+        System.out.println("Данные ФИО сотрудника — " + fullName);
     }
 }
